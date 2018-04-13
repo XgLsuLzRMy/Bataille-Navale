b53f292d613b1820560292abb76ea171267ac4ef
saraben28 <33553596+saraben28@users.noreply.github.com>
2018-04-13 10:37:43 +0200

    Update Bateau.hpp

--- src/Bateau.hpp
+++ src/Bateau.hpp
@@ -13,20 +13,96 @@
 
 class Bateau{
 	private:
-		int taille, xExtremite, yExtremite; // l'extremite est à gauche pour horizontal et en haut pour vertical
-		bool orientation; // 0=horizontal ; 1=vertical
+		int taille, xExtremite, yExtremite; /*!< Paramètres caractéristiques du bateau incluant sa taille et ses coordonnées (x,y) en extrimité qui est à gauche pour l'orientation horizontal et en haut pour l'orientation verticlal */
+	
+		bool orientation; /*!< orientation du bateau 0 pour l'horizontal et 1 pour le vertical */ 
 	public:
+	/**
+            * \fn public Bateau()
+	    *
+            * \brief Constructeur de la classe Bateau qui met ses caractéristques en ses valeurs par défaut
+	    *
+            */
 		Bateau();
+	
+	/**
+           * \fn public setTaille 
+	   *
+           * \brief fonction qui modifie l'attribut taille du bateau
+           *
+           * \param taille : taille voulue du bateau
+	   *
+           */
 		void setTaille(int taille);
+	
+	   /**
+           * \fn public setxExtremite  
+           * \brief fonction qui modifie l'attribut xExtremite (coordonnée en x) du bateau
+	   *
+           * \param xExtremite : coordonnée en x voulue du bateau
+	   *
+           */
+	
 		void setxExtremite(int xExtremite);
+	/**
+           * \fn public setyExtremite  
+	   *
+           * \brief fonction qui modifie l'attribut yExtremite (coordonnée en y) du bateau
+	   *
+           * \param xExtremite : coordonnée en y voulue du bateau
+        */
 		void setyExtremite(int yExtremite);
-		void setOrientation(char orientationInput);
-		
+	 /**
+            * \fn public setOrientation  
+	    *
+            * \brief fonction qui modifiE l'orientation du bateau. O pour une orientation horizontale et 1 pour la vertciale.
+	    *
+            * \param orientationInput : caractère h (pour horizontal) ou v (pour vertical) qui va être transformé en son booléen correspondant (0 pour h et 1 pour v). 
+       */
+	
+	void setOrientation(char orientationInput);
+	
+	/**
+            * \fn public getxExtremite  
+            * \brief fonction qui renvoie l'attribut xExtremite (coordonnée en x) d'un bateau
+            *
+            * \return la fonction retourne la coordonnée x du bateau. 
+            */
+	 
+	int getxExtremite();
+	
+	/**
+            * \fn public getyExtremite  
+            * \brief fonction qui renvoie l'attribut xExtremite (coordonnée en y) d'un bateau
+            *
+            * \return la fonction retourne la coordonnée en y du bateau.
+         */
+	 int getyExtremite();
+	 
+	/**
+           * \fn public getOrientation 
+           * \brief fonction qui renvoie l'orientation du bateau en booléen ( 0 s'il est horizontal, 1 s'il est vertical)
+           *
+           * \return cette fonction retourne l'orientation du bateau.
+  
+         */
 		bool getOrientation();
+	
+	/**
+           * \fn public getTaille  
+           * \brief fonction qui renvoie la taille d'un bateau.
+           *
+           * \return cette fonction retourne la taille du bateau 
+        */
+
 		int getTaille();
-		int getxExtremite();
-		int getyExtremite();
 		
+	/**
+           * \fn public getCoordonneesCompletes
+           * \brief fonction renvoie tous les coordonnées en x et y occupées par le bateau
+           *
+           * \return cette fonction retourne les coordonnées entiers (x,y) du bateau;
+        */		
 		int** getCoordonneesCompletes();
 };
 
