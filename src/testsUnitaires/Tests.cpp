ba083fe7676fc5b6cee5027281ee0a513dcc89b3
XgLsuLzRMy <lucas.ramage@insa-rouen.fr>
2018-04-20 09:50:38 +0200

    Debut tests unitaires

---
+++ src/testsUnitaires/Tests.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+#include <netinet/in.h>
+
+#include "../Grille.hpp"
+#include "../Joueur.hpp"
+#include "../JeuBatailleNavale.hpp"
+#include "../Bateau.hpp"
+
+using namespace CppUnit;
+using namespace std;
+
+//-----------------------------------------------------------------------------
+
+class Tests : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(Tests);
+	CPPUNIT_TEST(testCheckFinJeu);
+	CPPUNIT_TEST(testPlacerBateau);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp(void);
+	void tearDown(void);
+protected:
+	void testplacerBateau(void);
+	void testCheckFinJeu(void);
+private:
+	Joueur *mTestJoueur;
+	JeuBatailleNavale* mTestJeu,
+	Grille* mTestGrille;
+	Bateau* mTestBateau;
+};
+//-----------------------------------------------------------------------------
+void Tests::testCheckFinJeu(void)
+{
+	Grille grilleFinie;
+	grilleFinie.reset(); // remplie de 0 donc finie
+	mTestJeuBatailleNavale->joueur1.setGrille(grilleFinie); // La grille du joueur 1 est finie
+	CPPUNIT_ASSERT(true == mTestJeuBatailleNavale->chekFinJeu());
+}
+void Tests::testPlacerBateau(void)
+{
+	
+}
+void Tests::setUp(void)
+{
+	mTestJeu = new JeuBatailleNavale();
+	mTestJoueur = new Joueur();
+	mTestGrille = new Grille();
+	mTestBateau = new Bateau();
+}
+void Tests::tearDown(void)
+{
+	delete mTestJeu;
+}
